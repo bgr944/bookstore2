996f7ebb3b71d21db6688ede9a41b5dc95f1506f
bgr944 <bgr944@myy.haaga-helia.fi>
2024-02-05 20:07:32 +0200

    entity, id, generated value for book class

--- src/main/java/hh_sof03/bookstore2/domain/Book.java
+++ src/main/java/hh_sof03/bookstore2/domain/Book.java
@@ -1,8 +1,18 @@
 package hh_sof03.bookstore2.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Book {
 
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private long id;
 	 	private String title, author, isbn;
 	    private int year;
 	    private double price;
@@ -20,7 +30,10 @@ public class Book {
 
 	   public Book(){}
 	 
-	   
+	   public long getId()
+	    {
+	        return id;
+	    }
 	   
 	    public String getTitle()
 	    {
@@ -40,8 +53,13 @@ public class Book {
 	        return isbn;
 	    } 
 	    public double getPrice()
+		{
+			return price;
+		}
+
+		public void setId(long id)
 	    {
-	        return price;
+	        this.id = id;
 	    }
 
 	    public void setTitle(String title)
