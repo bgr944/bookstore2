c8d3deffa9d023b2bd5b6f66f0360f4091663678
bgr944 <bgr944@myy.haaga-helia.fi>
2024-02-07 17:48:15 +0200

    listpage

--- src/main/java/hh_sof03/bookstore2/web/BookController.java
+++ src/main/java/hh_sof03/bookstore2/web/BookController.java
@@ -7,20 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import hh_sof03.bookstore2.domain.Book;
+import hh_sof03.bookstore2.domain.BookRepository;
 
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository BookRepository;
 
 	
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
     public String Books(Model model) {
-		
-		List<Book> Books = new ArrayList<Book>();
 
-		model.addAttribute("Books", Books);
+		model.addAttribute("Books", BookRepository.findAll());
 		
 		return "booklist";
 		
