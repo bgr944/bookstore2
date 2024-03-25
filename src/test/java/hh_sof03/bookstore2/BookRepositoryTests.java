9a0b54d8373edce4b61e656f5ba21b1084ad75f3
bgr944 <bgr944@myy.haaga-helia.fi>
2024-03-25 09:56:41 +0200

    Dockerfile added

---
+++ src/test/java/hh_sof03/bookstore2/BookRepositoryTests.java
@@ -0,0 +1,47 @@
+package hh_sof03.bookstore2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import hh_sof03.bookstore2.domain.Book;
+import hh_sof03.bookstore2.domain.Category;
+import hh_sof03.bookstore2.domain.BookRepository;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class BookRepositoryTests {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Test
+    public void findByIdShouldReturnBook() {
+        List<Book> books = repository.findByTitle("Big Scary Book");
+        
+        assertThat(books).hasSize(1);
+        assertThat(books.get(0).getIsbn()).isEqualTo("943-46575-8");
+    }
+    
+    @Test
+    public void createNewBook() {
+        Book book = new Book("943-46575-7", "Big Scary Book", "John Author", 2021, 15.00, null);
+        repository.save(book);
+        assertThat(book.getId()).isNotNull();
+    }
+
+
+    @Test
+    public void deleteEveryBook() {
+        repository.deleteAll();
+        assertThat(repository.count()).isEqualTo(0);
+    }
+}
--- src/test/java/hh_sof03/bookstore2/Bookstore2ApplicationTests.java
+++ src/test/java/hh_sof03/bookstore2/Bookstore2ApplicationTests.java
@@ -1,13 +1,23 @@
 package hh_sof03.bookstore2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import hh_sof03.bookstore2.web.BookController;
 
 @SpringBootTest
 class Bookstore2ApplicationTests {
 
+	@Autowired
+    private BookController controller;
+
 	@Test
-	void contextLoads() {
-	}
+	public void contexLoads() throws Exception {
+	assertThat(controller).isNotNull();
+ } 
 
 }
